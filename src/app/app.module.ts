5356fc7122f94df7bec02612110e0b65b037f5d9
Jop <jop.verhoeven@gmail.com>
2019-12-18 09:35:54 +0100

    Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatToolbarModule, MatMenuModule, MatButtonModule, MatCardModule, } from '@angular/material/';
-import { RouterModule, Routes } from '@angular/router';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { AboutComponent } from './about/about.component';
-import { MeComponent } from './about/me/me.component';
-import { FonstagramComponent } from './about/fonstagram/fonstagram.component';
-import { LollookupComponent } from './about/lollookup/lollookup.component';
-import { UiModule } from './ui/ui.module';
-
+import { DefaultModule } from './layouts/default/default.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent,
-    AboutComponent,
-    MeComponent,
-    FonstagramComponent,
-    LollookupComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
-
-
-
-    // Material Components
-    MatToolbarModule,
-    MatMenuModule,
-    MatButtonModule,
-    MatCardModule,
-    UiModule,
+    DefaultModule
   ],
   providers: [],
   bootstrap: [AppComponent]
